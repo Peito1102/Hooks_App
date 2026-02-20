aed1e83cf0fe7744e347f31cb4b010978cf81efc
Peito1102 <renzovasquezch@hotmail.com>
2026-02-19 20:18:41 -0500

    cap 12-8

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,13 +1,49 @@
-import { type PropsWithChildren } from "react"
+import { createContext, useState, type PropsWithChildren } from "react"
+import type { User } from "../data/user-mock.data";
 
 /* interface UserContextProp {
   children: React.ReactNode
 } */
 
+type AuthStatus = 'authenticated' | 'cheking' | 'not-authenticated'
+
+interface UserContextProp {
+  //sate
+  authStatus: AuthStatus;
+  user: User | null;
+
+  //methods
+  login: (userId: number) => boolean;
+  logout: () => void;
+}
+
+export const UserContext = createContext({} as UserContextProp);
+
 const UserContextProvider = ({ children } : PropsWithChildren) => {
+
+  const [authStatus, setAuthStatus] = useState<AuthStatus>('cheking');
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (userId: number) => {
+    console.log('Login with userId: ', userId);
+    return true;
+  }
+
+  const handleLogout = () => {
+    console.log('Logout');
+  }
+
   
   return (
-    <>{children}</>
+    <UserContext
+    value={{
+      authStatus: authStatus,
+      user: user,
+      login: handleLogin,
+      logout: handleLogout
+    }}>
+      {children}
+      </UserContext>
   )
 }
 
